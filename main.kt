5ff4f5477525304df271dcd78234d861261d920f
Pedro <izielucas2016@gmail.com>
2024-05-16 22:49:07 -0300

    Finished until 6º question

---
+++ main.kt
@@ -0,0 +1,70 @@
+fun atividade1(lista: List<Int>): Int {
+    return lista.filter { it % 2 == 0 }.sum();
+}
+
+fun atividade2(lista: List<String>): String? {
+    val contador = mutableMapOf<String, Int>();
+
+    for (string in lista) {
+        contador[string] = contador.getOrDefault(string, 0)+1;
+    }
+    var maisRepetida: String? = null;
+    var maxContagem = 0
+
+    for ((string, cont) in contador) {
+        if (cont > maxContagem) {
+            maisRepetida = string
+            maxContagem = cont
+        } else {
+            maisRepetida = "Não há string mais repetida"
+        }
+    }
+
+    return maisRepetida;
+}
+
+fun atividade3(senha: String): Boolean {
+    if (senha.length < 8) {
+        return false;
+    }
+
+    var validaSenha = false;
+
+    for (caracter in senha) {
+        when {
+            caracter.isUpperCase() && caracter.isLowerCase() && caracter.isDigit() -> validaSenha = true;
+        }
+    }
+
+    return validaSenha;
+}
+
+fun atividade4(lista: List<String>): MutableList<String> {
+    var maisQue5Digitos = listOf<String>().toMutableList();
+    for (string in lista) {
+        if (string.length > 5){
+            maisQue5Digitos.add(string);
+        }
+    }
+
+    return maisQue5Digitos;
+}
+
+fun atividade5(valor: Number): Float {
+    var precoFinal: Float;
+    val valorFloat = valor.toFloat();
+
+    if (valorFloat > 100) {
+        precoFinal = valorFloat - (valorFloat * 0.1f);
+    } else {
+        precoFinal = valorFloat;
+    }
+
+    return precoFinal;
+}
+
+
+fun main () {
+    var valor = 120;
+    println(atividade5(valor))
+}
